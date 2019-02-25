cc773bc0b797f2e5819b97dbf9ebb16bf8bcf355
jcastelain <jcastelain.ext@wedigital.garden>
2019-02-25 12:00:18 +0100

    Feat(Product): CRUD initialized and operational.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('products', 'ProductController@index');
+Route::post('product', 'ProductController@store');
+Route::get('product/{id}', 'ProductController@show');
+Route::put('product/{id}', 'ProductController@update');
+Route::delete('product/{id}', 'ProductController@delete');
